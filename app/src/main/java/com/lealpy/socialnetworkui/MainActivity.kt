ff41aa88d1b2dec87c4424cc7f1715de14cf05a1
lealpy <lealp@mail.ru>
2021-12-13 11:02:19 +0300

    predictor app done

--- app/src/main/java/com/lealpy/socialnetworkui/MainActivity.kt
+++ app/src/main/java/com/lealpy/socialnetworkui/MainActivity.kt
@@ -2,10 +2,52 @@ package com.lealpy.socialnetworkui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.lealpy.socialnetworkui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+
+        initViews()
+        initObservers()
+
+    }
+
+    private fun initViews() {
+
+        binding.getPredictionBtn.setOnClickListener {
+            viewModel.onBtnClicked()
+        }
+
+    }
+
+    private fun initObservers() {
+
+        viewModel.prediction.observe(this) { prediction ->
+            binding.predictionTV.text = prediction
+        }
+
+        viewModel.predictionVisibility.observe(this) { predictionVisibility ->
+            binding.predictionCV.visibility = predictionVisibility
+        }
+
+        viewModel.clickCounterText.observe(this) { clickCounter ->
+            binding.predictionCounterTV.text = clickCounter
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/lealpy/socialnetworkui/MainViewModel.kt
@@ -0,0 +1,50 @@
+package com.lealpy.socialnetworkui
+
+import android.app.Application
+import android.view.View
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.util.*
+
+class MainViewModel (application : Application) : AndroidViewModel(application) {
+
+    private val _prediction = MutableLiveData<String>()
+    val prediction : LiveData<String> = _prediction
+
+    private val _predictionVisibility = MutableLiveData(View.INVISIBLE)
+    val predictionVisibility : LiveData<Int> = _predictionVisibility
+
+    private var clickCounter = 0
+
+    private val _clickCounterText = MutableLiveData(
+        getApplication<Application>().resources.getString(R.string.got_predictions) + " 0"
+    )
+    val clickCounterText : LiveData<String> = _clickCounterText
+
+    private val firstPhrase = getApplication<Application>()
+        .resources
+        .getStringArray(R.array.firstPhrase)
+        .toList()
+
+    private val secondPhrase = getApplication<Application>()
+        .resources
+        .getStringArray(R.array.secondPhrase)
+        .toList()
+
+    fun onBtnClicked() {
+        _predictionVisibility.value = View.VISIBLE
+        clickCounter ++
+        _clickCounterText.value = getApplication<Application>()
+            .resources.getString(R.string.got_predictions) + " " + clickCounter
+        generatePhrase()
+    }
+
+    private fun generatePhrase() {
+        val random = Random()
+        val indexOfFirstPhrase = random.nextInt(firstPhrase.lastIndex + 1)
+        val indexOfSecondPhrase = random.nextInt(secondPhrase.lastIndex + 1)
+        _prediction.value = firstPhrase[indexOfFirstPhrase] + " " + secondPhrase[indexOfSecondPhrase]
+    }
+
+}
